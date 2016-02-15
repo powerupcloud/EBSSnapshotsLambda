46db5b2da38bee940db484cd195012b0066eab8d
Powerupcloud <powerupcloud@users.noreply.github.com>
2016-02-15 16:18:44 +0530

    Create delete_snapshots.py

---
+++ delete_snapshots.py
@@ -0,0 +1,28 @@
+import boto3
+import re
+import datetime
+
+#Please mention your region name
+#below line code is call cross region
+ec = boto3.client('ec2', region_name='us-west-2')
+iam = boto3.client('iam')
+
+#begins lambda function
+def lambda_handler(event, context):
+    account_ids = list()
+    try:         
+        iam.get_user()
+    except Exception as e:
+        # use the exception message to get the account ID the function executes under
+        account_ids.append(re.search(r'(arn:aws:sts::)([0-9]+)', str(e)).groups()[1])
+
+    delete_on = datetime.date.today().strftime('%Y-%m-%d')
+    filters = [
+        {'Name': 'tag-key', 'Values': ['DeleteOn']},
+        {'Name': 'tag-value', 'Values': [delete_on]},
+    ]
+    snapshot_response = ec.describe_snapshots(OwnerIds=account_ids, Filters=filters)
+
+    for snap in snapshot_response['Snapshots']:
+        print "Deleting snapshot %s" % snap['SnapshotId']
+        ec.delete_snapshot(SnapshotId=snap['SnapshotId'])
